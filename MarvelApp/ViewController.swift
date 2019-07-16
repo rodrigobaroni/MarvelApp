08b9316f14bda87f5162526056e0dfcfa20e7462
Rodrigo Baroni <rodrigoalexisgarciabaroni@correio.itau.com.br>
2019-07-16 13:12:25 -0300

    Initial Commit

---
+++ MarvelApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelApp
+//
+//  Created by Rodrigo Alexis Garcia Baroni on 16/07/2019.
+//  Copyright © 2019 Rodrigo Alexis Garcia Baroni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
